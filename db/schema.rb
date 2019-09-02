c17a120b79402b4587c864c74bb505cdf8b4be90
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-09-02 01:57:21 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_010438) do
+ActiveRecord::Schema.define(version: 2019_09_02_014353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_010438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published", default: false
-    t.string "slug"
     t.integer "user_id"
+    t.string "slug"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
