9896fbb3093d4524c5de898b9adc089a4c856fb9
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-11-17 16:11:16 -0500

    Code scaffold is ready to go

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def index
     @articles = Article.all
     @projects = Project.all
+    @code_samples = CodeSample.all
   end
   def about
   end
--- app/models/code_sample.rb
+++ app/models/code_sample.rb
@@ -1,5 +1,6 @@
 class CodeSample < ApplicationRecord
   belongs_to :user
+  has_rich_text :code_info
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
