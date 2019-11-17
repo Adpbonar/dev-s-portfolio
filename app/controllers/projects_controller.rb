50a8b711ab08fb324f68dbb3dc0ee0b358c57b20
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-11-17 17:36:56 -0500

    Adding Live Link to Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -71,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :featured, :versions, :project_info, :project_image, :description, :coming_soon, :GitHub_link)
+      params.require(:project).permit(:title, :featured, :versions, :project_info, :project_image, :description, :coming_soon, :GitHub_link, :live_link)
     end
 end
