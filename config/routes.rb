72cb7c456dbe7692fa62e33415c674e6db7cf2b1
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2020-01-26 11:06:20 -0500

    Testing out http to https

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+match "*path" => redirect("https://www.devanpotterbonar.com/%{path}"), :constraints => { :protocol => "http://" }
+match "*path" => redirect("https://www.devanpotterbonar.com/%{path}"), :constraints => { :subdomain => "" }
 Rails.application.routes.draw do
   resources :articlesend
   resources :index_documents, only: [:index, :edit, :update]
