d8b9dfb721cc409c8e19b93460dff0d4b788cba1
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2020-02-06 12:00:48 -0500

    Adding validations to programs

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,10 @@ class Project < ApplicationRecord
   belongs_to :user
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :versions, presence: true
+  validates :description, presence: true, length: { minimum: 3, maximum: 10000 }
+  validates :GitHub_link, presence: true
+  
 end
