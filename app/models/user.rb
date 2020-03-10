7937d128be813ff06b658630f1e1013579dc515d
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2020-03-09 22:06:25 -0400

    Adding password complexity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  validates :password_complexity
+  validate :password_complexity
+
   has_many :articles
   has_many :projects
   has_many :code_samples
@@ -7,4 +11,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    errors.add :password, 'Complexity requirement not met. Please use at least: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
